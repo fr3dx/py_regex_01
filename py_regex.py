492836af9d0480d505068cb144f979901cb6d814
fr3dx <77107619+fr3dx@users.noreply.github.com>
2022-09-06 10:14:41 +0200

    v05

--- py_regex.py
+++ py_regex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.8
 
-# Script filters out pattern(s), with the help of regex from opened file: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX 
-# and format them such: ('XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX','XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX')
+# Script filters out pattern(s): XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX with the help of regex from opened file and 
+# format them such: ('XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX','XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX')
 
 import re
 
