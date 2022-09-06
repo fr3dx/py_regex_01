5a581d96d8f175c00c906e545c2488e1217ddcea
fr3dx <77107619+fr3dx@users.noreply.github.com>
2022-09-06 10:12:50 +0200

    v04

--- py_regex.py
+++ py_regex.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.8
 
-# Script filters out pattern, with the help of regex from opened file: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX and format them such: ('XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX','XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX')
+# Script filters out pattern(s), with the help of regex from opened file: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX 
+# and format them such: ('XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX','XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX')
 
 import re
 
